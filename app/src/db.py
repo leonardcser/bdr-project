5686e3c936fd1a8f6c7209c187afca5cd144f3ee
leonardcser <73912641+leonardcser@users.noreply.github.com>
2024-12-15 17:47:35 +0100

    Simplify seed

--- app/src/db.py
+++ app/src/db.py
@@ -1,8 +1,8 @@
 import asyncio
-from pathlib import Path
+
+from databases import Database
 
 from config import DATABASE_URL
-from databases import Database
 
 database = Database(DATABASE_URL)
 
@@ -15,17 +15,3 @@ async def connect():
         except ConnectionRefusedError:
             print("Database connection failed. Retrying in 5 seconds...")
             await asyncio.sleep(5)
-
-
-async def seed_database():
-    sql_file_path = Path("src/sql/schema.sql")
-
-    with open(sql_file_path, "r") as f:
-        sql_commands = f.read()
-
-    try:
-        async with database.transaction():
-            await database.execute(sql_commands)
-        print("Database seeded successfully.")
-    except Exception as e:
-        print(f"Error during database seeding: {e}")
--- app/src/main.py
+++ app/src/main.py
@@ -1,13 +1,14 @@
-from config import hotreload
-from db import connect, database, seed_database
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from routes import router
 from starlette.routing import WebSocketRoute
+
+from config import hotreload
+from db import connect, database
+from routes import router
 
 app = FastAPI(
     routes=[WebSocketRoute("/hot-reload", hotreload, name="hot-reload")],
-    on_startup=[hotreload.startup, connect, seed_database],
+    on_startup=[hotreload.startup, connect],
     on_shutdown=[hotreload.shutdown, database.disconnect],
 )
 app.mount("/static", StaticFiles(directory="static"), name="static")
