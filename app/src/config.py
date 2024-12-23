abff064af33db5b082a6a70231b0d6453eab982f
leonardcser <73912641+leonardcser@users.noreply.github.com>
2024-12-23 14:24:09 +0100

    Add active links

--- app/src/config.py
+++ app/src/config.py
@@ -1,5 +1,6 @@
 import arel
 from fastapi.templating import Jinja2Templates
+from jinja2 import pass_context
 
 DEVELOPMENT = 1
 DATABASE_URL = "postgresql://bdr:bdr@database:5432/db"
@@ -12,5 +13,14 @@ hotreload = arel.HotReload(
 )
 
 templates = Jinja2Templates(directory="src/templates")
+
+
+@pass_context
+def is_current(context, path):
+    request = context["request"]
+    return "current" if request.url.path == path else ""
+
+
+templates.env.globals["is_current"] = is_current
 templates.env.globals["DEVELOPMENT"] = DEVELOPMENT
 templates.env.globals["hotreload"] = hotreload
